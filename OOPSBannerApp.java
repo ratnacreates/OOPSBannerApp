5fe501af03313f3ac271f6df95f159414b1ec0e6
ratnacreates <ratnapriya120512@gmail.com>
2026-02-17 09:30:37 +0530

    UCL Banner Code Added

---
+++ OOPSBannerApp.java
@@ -0,0 +1,21 @@
+/**
+ * OOPSBannerApp UC1 - OOPS BANNER DISPLAY APPLICATION 
+ * 
+ * THIS class demonstrates a simple Java application that display the object 
+ * Oriented programming system OOPS acronym to the console.
+ * @author Ratna
+ * @version 1.0
+ */
+
+public class OOPSBannerApp {
+
+    
+    public static void main(String[] args) 
+    {
+        System.out.println("OOPS");
+    }
+
+}
+
+    
+
